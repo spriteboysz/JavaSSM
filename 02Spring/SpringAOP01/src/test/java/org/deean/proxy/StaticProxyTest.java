638199a6370f69a9f24208b574b965311651f387
spriteboysz <sprite_bx@126.com>
2023-11-11 23:09:20 +0800

    2023111123

---
+++ 02Spring/SpringAOP01/src/test/java/org/deean/proxy/StaticProxyTest.java
@@ -0,0 +1,22 @@
+package org.deean.proxy;
+
+import junit.framework.TestCase;
+import org.deean.dao.BookDAOImpl;
+import org.deean.dao.StudentDAOImpl;
+
+public class StaticProxyTest extends TestCase {
+
+    public void testInsert() {
+        BookDAOImpl bookDAO = new BookDAOImpl();
+        StaticProxy proxy = new StaticProxy(bookDAO);
+        proxy.insert();
+        proxy.delete();
+        proxy.update();
+        StudentDAOImpl studentDAO = new StudentDAOImpl();
+        proxy = new StaticProxy(studentDAO);
+        proxy.insert();
+        proxy.delete();
+        proxy.update();
+    }
+
+}
