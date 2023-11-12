b95169825ef2dc5a56242fc80712230f17042027
spriteboysz <sprite_bx@126.com>
2023-11-12 21:01:51 +0800

    2023111221

---
+++ 02Spring/SpringAOP01/src/main/java/org/deean/utils/LogManager.java
@@ -0,0 +1,16 @@
+package org.deean.utils;
+
+import java.util.Date;
+
+/**
+ * Author: Deean
+ * Date: 2023-11-12 20:55
+ * FileName: src/main/java/org/deean/utils
+ * Description:
+ */
+
+public class LogManager {
+    public void printLog(){
+        System.out.println("---" + new Date() + " log---");
+    }
+}
