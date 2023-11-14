8c7ba08b0753ec26e7ab2cef9d2f051070e1ec4a
spriteboysz <sprite_bx@126.com>
2023-11-15 00:08:15 +0800

    2023111500

---
+++ 03SpringMVC/SpringMVCBasic/src/main/java/com/deean/springmvcbasic/controllers/BookController.java
@@ -0,0 +1,17 @@
+package com.deean.springmvcbasic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Author: Deean
+ * Date: 2023-11-15 00:02
+ * FileName: src/main/java/com/deean/springmvcbasic/controllers
+ * Description:
+ */
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+}
---
+++ 03SpringMVC/SpringMVCBasic/src/main/java/com/deean/springmvcbasic/pojo/Book.java
@@ -0,0 +1,12 @@
+package com.deean.springmvcbasic.pojo;
+
+/**
+ * Author: Deean
+ * Date: 2023-11-15 00:07
+ * FileName: src/main/java/com/deean/springmvcbasic/pojo
+ * Description:
+ */
+
+public class Book {
+
+}
